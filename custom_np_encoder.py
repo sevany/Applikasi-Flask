a7aee8d13eb346f5df481db46a8c769b78ca5f05
Myra <47497128+sevany@users.noreply.github.com>
2021-06-29 12:59:40 +0800

    Add files via upload

---
+++ custom_np_encoder.py
@@ -0,0 +1,10 @@
+import json
+from json import JSONEncoder
+import numpy as np
+
+class NumpyArrayEncoder(JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, np.ndarray):
+            return obj.tolist()
+        else:
+            return JSONEncoder.default(self, obj)
